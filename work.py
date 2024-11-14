0c3da3d1bc7a1d8ab6ac5c8463edcad4ee1276cf
signedana24 <anfe0883@student.ssttx.org>
2024-11-14 13:47:02 -0600

    modified:   work.py

--- work.py
+++ work.py
@@ -1,2 +1,31 @@
 #twting
-print("hello")
+# Enter your code here
+num1= int(input("Choose a number"))
+num2= int(input("Choose a number"))
+sumOfnums = 0
+
+for i in range(num1, num2+1):
+	sumOfnums += i
+
+print(sumOfnums)
+
+
+
+# Enter your code here
+n= int(input("value of i"))
+sumOfN= 1
+
+for i in range(1,n+1):
+    sumOfN*=i
+    
+print(sumOfN)
+
+
+
+
+dice = ["1","2","3","4","5","6"]
+def allRolls():
+    for i in dice:
+        for j in dice:
+            print(i + "," + j)
+allRolls()
